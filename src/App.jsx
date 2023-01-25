cad2726db5027279a57045dd880f6c48d117740e
Lucas Carvalho <lucasilva1990@gmail.com>
2023-01-25 18:53:16 -0300

    Route to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import NavBar from './components/NavBar-index'
 
 function App() {
 
   return (
     <div className = "App">
     
+      < NavBar />
+      
       <div className = 'container'>
         <Outlet/>
       </div>
---
+++ src/components/NavBar-index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <nav className = 'navbar-class'>
+
+        <h2> 
+            <Link to = {'/'}>It's not a blog</Link>
+        </h2>
+
+        <ul>
+            <li>
+                <Link to = {'/'}>Home</Link>
+            </li>
+            <li>
+                <Link to = {'/new'} className = 'newpost-class'>New Post</Link>
+            </li>
+        </ul>
+
+    </nav>
+  )
+}
+
+export default NavBar
