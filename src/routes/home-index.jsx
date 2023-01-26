3bcc1beba0c7c53978deb84b3be90c83ce504bef
Lucas Carvalho <lucasilva1990@gmail.com>
2023-01-25 21:06:47 -0300

    Comunication with API

--- src/routes/home-index.jsx
+++ src/routes/home-index.jsx
@@ -1,12 +1,49 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './home-style.css'
+import axios from 'axios'
 
 const Home = () => {
+
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async() => {
+    
+    try {
+
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+
+      const data = response.data
+      console.log (response.data)
+      setPosts(response.data)
+      console.log (posts.length)
+
+    } catch (error) {
+      console.log (error)
+    }
+
+  }
+
+  // Execute just one time
+  useEffect(() => {
+    getPosts()
+  }, [])
+
+  
   return (
-    <>
-        <div>
-            <h1>Home</h1>
-        </div>
-    </>
+    <div>
+      <h1>Lasts posts: </h1>
+      {posts.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        posts.map((post) => (
+          <div className = 'post' key = {post.id}>
+            <h2>{post.title}</h2>
+          </div>
+        ))
+      )}
+    </div>
   )
 }
 
