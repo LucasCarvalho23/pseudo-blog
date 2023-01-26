e2458ffe233cf9bb60a4860c753bc656eb7f6c87
Lucas Carvalho <lucasilva1990@gmail.com>
2023-01-25 22:42:17 -0300

    Project done

--- src/routes/newPost-index.jsx
+++ src/routes/newPost-index.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
 import './newPost-style.css'
+import blogFetch from '../axios/config'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const newPost = () => {
+
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+
+  const createPost = async (e) => {
+    e.preventDefault(e)
+
+    const post = {title, body, userId: 1}
+
+    await blogFetch.post("/posts", {
+      body: post,
+    })
+
+    navigate("/")
+  }
+
   return (
     <div className = 'new-post-class'>
       <h2>Insert new post</h2>
-      <form>
+      <form onSubmit = { (e) => createPost(e) }>
+
         <div className = 'form-control-class'>
           <label htmlFor=' = "title-class'>Title</label>
-          <input type="text" name = 'title-class' placeholder = 'Type a title' id = "title-id" />
-        </div>
-        <div className='form-control-class'>
+          <input type="text" name = 'title-class' placeholder = 'Type a title' id = "title-id" onChange = { (e)=> setTitle(e.target.value) } />
+        
           <label htmlFor=' = "body-class'>Content: </label>
-          <textarea name = 'body-class' placeholder = 'Type a post' id = "body-id" cols = "30" rows = "10" />
+          <textarea name = 'body-class' placeholder = 'Type a post' id = "body-id" cols = "30" rows = "10" onChange = { (e)=> setBody(e.target.value) } />
         </div>
+
         <p>
           <input type = "submit" value = "CreatePost" className = 'btn-class'></input>
         </p>
