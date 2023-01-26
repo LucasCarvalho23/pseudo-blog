1cec728b93a41d829bcb6f68a262104f8b41c93e
Lucas Carvalho <lucasilva1990@gmail.com>
2023-01-25 21:47:37 -0300

    CSS style final

--- src/routes/home-index.jsx
+++ src/routes/home-index.jsx
@@ -11,13 +11,10 @@ const Home = () => {
   const getPosts = async() => {
     
     try {
-
+      
       const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
-
       const data = response.data
-      console.log (response.data)
       setPosts(response.data)
-      console.log (posts.length)
 
     } catch (error) {
       console.log (error)
@@ -33,13 +30,15 @@ const Home = () => {
   
   return (
     <div>
-      <h1>Lasts posts: </h1>
+      <h1>Lasts posts</h1>
       {posts.length === 0 ? (
-        <p>Loading...</p>
+        <p>No file...</p>
       ) : (
         posts.map((post) => (
-          <div className = 'post' key = {post.id}>
+          <div className = 'post-class' key = {post.id}>
             <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to = {'/posts/${post.id}'} className = 'btn-class'>Read more</Link>
           </div>
         ))
       )}
